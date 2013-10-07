85088ace330f3a59bd5b52c25c041776c85b0099
M0P <kommt-nicht-an@web.de>
2013-10-07 16:32:03 +0200

    improvements

--- src/Raviolita.java
+++ src/Raviolita.java
@@ -1,33 +1,37 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Marc
- * Date: 07.10.13
- * Time: 10:28
- * To change this template use File | Settings | File Templates.
- */
 public class Raviolita {
-    private int laenge;
-    private int breite;
-    private double laengeDiagonale;
-    private double durchmesserBoden;
-    private double flaecheBoden;
-    private double flaecheMantel;
-    private double flaecheDose;
-    private double volumenDose;
+
+    private int     laenge;
+    private int     höhe;
+    private double  laengeDiagonale;
+    private double  durchmesserBoden;
+    private double  flaecheBoden;
+    private double  flaecheMantel;
+    private double  flaecheDose;
+    private double  volumenDose;
     private Scanner scanner;
 
     public Raviolita() {
         laenge = readInt("Gib bitte die Länge der Dose an:");
-        breite = readInt("Gib bitte die Breite der Dose an:");
-        System.out.println("Die Länge der Diagonalen des Blechstücks beträgt: " + Math.sqrt(laenge * laenge + breite * breite));
-        System.out.println("Der Durchmesser des Dosenbodens ist: " + laenge / Math.PI);
-        System.out.println("Die Fläche des Doesenbodens beträgt: " + Math.PI * (laenge / Math.PI / 2) * (laenge / Math.PI / 2));
-        System.out.println("Die Mantelfläche der Dose ist: " + laenge * breite);
-        System.out.println("Die Gesamtfläche der Dose beträgt:" + (laenge * breite + 2 * Math.PI * (laenge / Math.PI / 2) * (laenge / Math.PI / 2)));
-        System.out.println("Das Volumen der Dose ist: " + Math.PI * (laenge / Math.PI / 2) * (laenge / Math.PI / 2) * breite);
+        höhe = readInt("Gib bitte die Breite der Dose an:");
+
+        //Berechnungen
+        laengeDiagonale = Math.sqrt(laenge * laenge + höhe * höhe);
+        durchmesserBoden = laenge / Math.PI;
+        flaecheBoden=Math.PI * (durchmesserBoden / 2) * (durchmesserBoden / 2);
+        flaecheMantel=laenge * höhe;
+        flaecheDose=flaecheMantel+2*flaecheBoden;
+        volumenDose=flaecheBoden* höhe;
+
+        //Ausgaben
+        System.out.println("Die Länge der Diagonalen des Blechstücks beträgt: " +laengeDiagonale);
+        System.out.println("Der Durchmesser des Dosenbodens ist: " + durchmesserBoden);
+        System.out.println("Die Fläche des Doesenbodens beträgt: " +flaecheBoden);
+        System.out.println("Die Mantelfläche der Dose ist: " +flaecheMantel);
+        System.out.println("Die Gesamtfläche der Dose beträgt:" + flaecheDose);
+        System.out.println("Das Volumen der Dose ist: " + volumenDose);
     }
 
     public static void main(String[] args) {
